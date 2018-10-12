b3dd44abe96e25e63f52a9a243a9577d13db279c
Tyler Ketron <tketron@gmail.com>
2018-10-12 10:42:15 -0700

    added unit tests for AddStringPage reducers

--- app/containers/AddStringPage/tests/reducer.test.js
+++ app/containers/AddStringPage/tests/reducer.test.js
@@ -1,8 +1,14 @@
-import { fromJS } from 'immutable';
 import addStringPageReducer from '../reducer';
+import { addNewString } from '../actions';
 
 describe('addStringPageReducer', () => {
-  xit('returns the initial state', () => {
-    expect(addStringPageReducer(undefined, {})).toEqual(fromJS({}));
+  it('returns the initial state', () => {
+    expect(addStringPageReducer(undefined, {})).toMatchSnapshot();
+  });
+
+  it('handles the addString action', () => {
+    expect(
+      addStringPageReducer(undefined, addNewString('test string')),
+    ).toMatchSnapshot();
   });
 });
